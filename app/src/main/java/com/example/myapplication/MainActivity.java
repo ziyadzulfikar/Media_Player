f3ae94bba53dec3c092f74ceef5b12a8c2549b7c
Ziyad <ziyad.zulfiker@gmail.com>
2020-04-24 09:24:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button play,stop,pause;
+    MediaPlayer mediaPlayer;
+    int pauseCurrentPosition;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play=(Button) findViewById(R.id.btn_play);
+        stop=(Button) findViewById(R.id.btn_stop);
+        pause=(Button) findViewById(R.id.btn_pause);
+        play.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        pause.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_play:
+                if (mediaPlayer==null) {
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+                    mediaPlayer.start();
+                }
+                else if (!mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+                }
+                break;
+
+            case R.id.btn_pause:
+                if(mediaPlayer!=null) {
+                    mediaPlayer.pause();
+                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                }
+                break;
+
+            case R.id.btn_stop:
+                if (mediaPlayer!=null)
+                mediaPlayer.stop();
+                mediaPlayer=null;
+                break;
+        }
+    }
+}
